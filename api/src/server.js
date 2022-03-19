f74f08e19a4808f5cc6e39ed4b2adb4c2edc2a7d
Paulo Victor Alvares <pvsfair@gmail.com>
2022-03-19 17:29:30 -0300

    feat: initial commit for node express server

---
+++ api/src/server.js
@@ -0,0 +1,11 @@
+import app from './app.js';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(`Error trying to listen on port ${port}`);
+    console.error(err);
+  }
+  console.log(`Listening on port ${port}...`);
+});
